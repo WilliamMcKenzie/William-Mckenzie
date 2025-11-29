0e55ec5084006392104f0936fd1e6b173e561085
William <williamqmckenzie@gmail.com>
2025-11-29 14:49:08 -0500

    Fixed tileblemd

--- gallery/terrain/terrain.js
+++ gallery/terrain/terrain.js
@@ -2,14 +2,13 @@ const Application = PIXI.Application
 const Texture = PIXI.Texture
 const Assets = PIXI.Assets
 
-const app = new Application
+let app = new Application
 globalThis.__PIXI_APP__ = app
 
 const tileSize = 8
 let tileSheet
 let tiles = []
 let tileQueue = []
-
 let tileVariations = [
     4,
     4,
@@ -21,6 +20,7 @@ let tileVariations = [
     7,
 ]
 
+let dragging = false
 let lookupTable = []
 
 for (var i = 0; i < 100000; i++) {
@@ -45,83 +45,48 @@ function codify(tileInt, variation = rand(5) % tileVariations[tileInt]) {
     return code + "_" + force2Digits(variation)
 }
 
-function checkAdjacentTiles(x, y) {
-    const tile = tiles[y][x]
-
-    if (tiles[y][x - 1] == tile) {
-        return false
-    }
-    if (tiles[y - 1][x] == tile) {
-        return false
-    }
-    if (tiles[y][x + 1] == tile) {
-        return false
-    }
-    if (tiles[y + 1][x] == tile) {
-        return false
-    }
-
-    return true
-}
-
 async function renderTiles() {
     const tileSheet = await Assets.load("tiles")
     const tilesContainer = new PIXI.ParticleContainer()
 
-    function addTile(y, x, tile) {
+    function addTile(x, y, tile) {
         const tileCode = codify(tile)
-
-        if (checkAdjacentTiles(x/8, y/8)) {
-            const blend1 = new PIXI.Particle({
-                texture: tileSheet.textures[codify(tile, 0)],
-                x : x - 2,
-                y : y - 2,
-                scaleX: 1.5,
-                scaleY: 1.5,
-                alpha: 0.2,
-            })
-            const blend2 = new PIXI.Particle({
-                texture: tileSheet.textures[codify(tile, 0)],
-                x : x - 1,
-                y : y - 1,
-                scaleX: 1.25,
-                scaleY: 1.25,
-                alpha: 0.3,
-            })
-            tilesContainer.addParticle(blend1)
-            tilesContainer.addParticle(blend2)
-        }
-
+        
+        const blend1 = new PIXI.Particle({
+            texture: tileSheet.textures[codify(tile, 0)],
+            x : x - 2,
+            y : y - 2,
+            scaleX: 1.5,
+            scaleY: 1.5,
+            alpha: 0.2,
+        })
+        const blend2 = new PIXI.Particle({
+            texture: tileSheet.textures[codify(tile, 0)],
+            x : x - 1,
+            y : y - 1,
+            scaleX: 1.25,
+            scaleY: 1.25,
+            alpha: 0.3,
+        })
         const tileParticle = new PIXI.Particle({
             texture: tileSheet.textures[tileCode],
             x,
             y,
         })
+        tilesContainer.addParticle(blend1)
+        tilesContainer.addParticle(blend2)
         tilesContainer.addParticle(tileParticle)
     }
 
-    const w = app.canvas.width
-    const h = app.canvas.height
-
-    for (let y = 0; y < h; y += 8) {
-        const indexY = y/8
-        const row = tiles[indexY]
-
-        if (row == undefined) {
-            tiles[indexY] = []
-        }
-        
-        for (let x = 0; x < w; x += 8) {
-            const indexX = x/8
-
-            if (tiles[indexY][indexX] != undefined) {
-                addTile(y, x, tiles[indexY][indexX])
-                tiles[indexY][indexX] = undefined
-            } else {
-                tiles[indexY][indexX] = undefined
-            }
-        }
+    for (let tile of tileQueue) {
+        let x = tile[0]
+        let y = tile[1]
+        let which = tile[2]
+        tiles[y][x] = which
+        addTile(x * 8, y * 8, which)
     }
+    
+    tileQueue = []
     app.stage.addChild(tilesContainer)
 }
 
@@ -146,23 +111,85 @@ async function init() {
     app.ticker.add(renderTiles)
 }
 
-init()
-
-
-let dragging = false
-document.addEventListener("mousedown", () => {
-    dragging = true
-})
-document.addEventListener("mouseup", () => {
+async function resetCanvas() {
+    if (app != undefined) {
+        app.stage.removeChildren()
+    }
     dragging = false
-})
-document.addEventListener("mousemove", (e) => {
-    let x = Math.floor(e.x / 48)
-    let y = Math.floor(e.y / 48)
+    tiles = []
+    await init()
+    resizeCanvas()
+}
+
+async function resizeCanvas() {
+    const w = app.canvas.width
+    const h = app.canvas.height
+
+    for (let y = 0; y < h; y += 8) {
+        const indexY = y/8
+        const row = tiles[indexY]
+
+        if (row == undefined) {
+            tiles[indexY] = []
+        }
+        
+        for (let x = 0; x < w; x += 8) {
+            const indexX = x/8
+
+            if (tiles[indexY][indexX] != undefined) {
+                continue
+            } else {
+                tiles[indexY][indexX] = undefined
+            }
+        }
+    }
+}
+
+resetCanvas()
+
+
+
+
+
+
+
+
+window.addEventListener('resize', resetCanvas)
+document.addEventListener("mousedown", (e) => {
+    dragging = true
+    let scale = (app.stage.scale.x * 8)
+    let x = Math.floor(e.x / scale)
+    let y = Math.floor(e.y / scale)
 
     if (dragging && (tiles[y][x] != selectedTile)) {
         if (e.target.nodeName == "CANVAS") {
             tileQueue.push([x,y,selectedTile])
         }
+    }
+})
+document.addEventListener("mouseup", () => {
+    dragging = false
+})
+document.addEventListener("mousemove", (e) => {
+    let scale = (app.stage.scale.x * 8)
+    let x = Math.floor(e.x / scale)
+    let y = Math.floor(e.y / scale)
+
+    if (dragging && (tiles[y][x] != selectedTile)) {
+        if (e.target.nodeName == "CANVAS") {
+            tileQueue.push([x,y,selectedTile])
+        }
+    }
+})
+
+document.addEventListener('keydown', function(event) {
+    const key = event.key
+    let scale = app.stage.scale.x
+
+    if (key === '+' || key === '=' || key === 'Add' || (event.shiftKey && key === '=')) {
+        app.stage.scale = Math.max(1, scale + 1)
+    }
+    else if (key === '-' || key === 'Subtract' || key === '_') {
+        app.stage.scale = Math.max(1, scale - 1)
     }
 })
